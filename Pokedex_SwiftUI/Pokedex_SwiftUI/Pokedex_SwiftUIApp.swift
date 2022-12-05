d5cbd9c81669fae32dd3f1dcaf0e2baf8cd18d6b
mehrdadjdev <mehrdadj.mech@gmail.com>
2022-12-05 18:24:56 +0100

    create a new project to build Pokedex app in SwiftUI

---
+++ Pokedex_SwiftUI/Pokedex_SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex_SwiftUI
+//
+//  Created by MehrDad on 05.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
